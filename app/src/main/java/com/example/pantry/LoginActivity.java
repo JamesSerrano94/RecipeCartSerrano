4e1b152c56053a9a08c2068d1c827f35e77cc99d
mbrowne4 <mattbrowne123@gmail.com>
2022-04-13 11:21:01 -0400

    Implemented Login, Registration. Need to change to allow username. Started Options page.

---
+++ app/src/main/java/com/example/pantry/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.pantry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button btnLogin;
+    private TextView textRegister;
+    private EditText password,email;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin = (Button) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.login_password);
+        email = (EditText) findViewById(R.id.login_email);
+        textRegister = (TextView) findViewById(R.id.text_register);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                login();
+            }
+        });
+
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        });
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login(){
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.isEmpty()){
+            email.setError("Email can not be empty");
+            if(pass.isEmpty()){
+                password.setError("Password can not be empty");
+            }
+        }
+        else if(pass.isEmpty()){
+            password.setError("Password can not be empty");
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+}
